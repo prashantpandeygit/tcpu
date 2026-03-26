4d073708252f3c74ccd65d4b3b0732286670d39d
Prashant Pandey <prashantpandeyiuet@gmail.com>
2026-03-26 21:24:45 +0530

    ALU: operations

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -5,4 +5,15 @@ module ALU (input [31:0] in1, in2,
 
     always @(*)
     begin
-      
+        case(alu_control)
+
+          4'b0000: alu_result = in1&in2;
+          4'b0001: alu_result = in1|in2;
+          4'b0010: alu_result = in1+in2;
+          4'b0100: alu_result = in1-in2;
+          4'b1000: begin
+            if(in1<in2)
+            alu_result = 1;
+            else
+            alu_result = 0;
+          end
